86ffb6b3522de9681504bc20d9b912602df4cc10
Manish Kumar Gupta <manishgu231@gmail.com>
2024-08-01 01:48:07 +0530

    FIX : Navbar text typo corrected

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className=" w-full max-w-screen-xl flex justify-between px-2 py-4 ">
       <h1 className="text-4xl font-bold text-primary">
-        <Link href="/"> Stynx</Link>
+        <Link href="/"> Styx</Link>
       </h1>
 
       <GoToCartBtn />
